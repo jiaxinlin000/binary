3f1c88672d89a1b6b3686205dbc4731b8d92f3a3
Jiaxin.Lin <Jiaxin.Lin@MU-C3-10.mullen.pvt.k12.co.us>
2014-12-16 13:35:02 -0700

    first draft

---
+++ trace/src/trace/Binary.java
@@ -0,0 +1,82 @@
+package trace;
+import java.util.Collections;
+import java.util.*;
+import java.util.Scanner; 
+import java.util.ArrayList; 
+public class Binary
+	{
+public static int numberIn; 
+	public static void main(String[] args)
+		{
+	
+		ArrayList myArray= new ArrayList(); 
+		boolean answerYes= true; 
+		do
+			{
+			System.out.println("Enter a number between 1 and 20 please.");
+			Scanner input= new Scanner(System.in); 
+			
+			
+			if(input.hasNextInt())
+				{
+				numberIn= input.nextInt(); 
+				boolean intInRange= numberIn<=20&&numberIn>=1;
+				boolean end= numberIn==-1; 
+				while(!intInRange&&!end)
+					{
+					System.out.println("Enter a right number please.");
+					numberIn= input.nextInt(); 
+					intInRange= numberIn<=20&&numberIn>=1;
+					}
+				if(intInRange)
+				{myArray.add(numberIn);} 
+				if(end)
+					{
+					answerYes=false; 
+					
+					}
+
+				}
+			else
+				{
+				System.out.println("You should put in a number, try again!");
+				}
+						
+			
+			Collections.sort(myArray);
+			}while(answerYes);
+		for(int i=0; i<myArray.size();i++)
+			{
+			System.out.print(myArray.get(i)+" ");
+			
+			}
+		System.out.println();
+		//make the arrayList
+		
+		int targetIndex= (int)(Math.random()*myArray.size());
+		int target= (int)myArray.get(targetIndex);
+		
+		System.out.println("Your target is "+target);
+		// target 
+		
+		boolean finish = true; 
+		int finalNumber;
+		int totalQ=0;
+		int correct=0; 
+		int left=0;
+		int right=0; 
+		do
+			{
+			
+			
+			
+			
+			}while(!finish); 
+		
+		
+		
+		
+		}
+	
+	
+	}
